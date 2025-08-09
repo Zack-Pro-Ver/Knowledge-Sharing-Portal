51625596bba1b14e2281e7ee6e03b2f2aaede0dd
Sindhu <sindhul25072002@gmail.com>
2025-08-09 20:32:23 +0530

    installed packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import BarChartComponent from "./components/BarChartComponent";
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-gradient-to-tr from-blue-400 to-purple-500">
-      <h1 className="text-4xl font-bold text-green-500">Welcome to Tailwind CSS + Vite + React!</h1>
+    <div className="App">
+      <h1>React Chart Example</h1>
+      <BarChartComponent />
     </div>
   );
 }
