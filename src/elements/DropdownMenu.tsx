4ef66c2577d87231a7b5d10742414c3d5c515efe
Sindhu <sindhul25072002@gmail.com>
2025-08-11 18:18:49 +0530

    elements dropdown and iconbutton

--- src/elements/DropdownMenu.tsx
+++ src/elements/DropdownMenu.tsx
@@ -4,10 +4,8 @@ interface DropdownMenuProps {
   isOpen: boolean;
 }
 
-const menuContainerClass =
-  "absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg focus:outline-none z-20";
-const menuItemClass =
-  "block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100";
+const menuContainerClass ="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg focus:outline-none z-20";
+const menuItemClass = "block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100";
 
 function DropdownMenu({ isOpen, children }: PropsWithChildren<DropdownMenuProps>) {
   if (!isOpen) return null;
