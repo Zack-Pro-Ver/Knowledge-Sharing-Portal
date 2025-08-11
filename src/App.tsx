fa89c90a16468f58958e0bff07fe6a8a5737c7a0
Sindhu <sindhul25072002@gmail.com>
2025-08-11 18:45:13 +0530

    removed href tag and added link to, to prevent refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
-// import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <> <Navbar />  
-     </>
+      <BrowserRouter>
+ <Navbar />  
+     </BrowserRouter>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom"; // New import
 import { BellIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import IconButton from "../elements/IconButton";
 import DropdownMenu, { menuItemClass } from "../elements/DropdownMenu";
@@ -11,9 +12,8 @@ function Navbar() {
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
         <div className="flex items-center justify-between">
           {/* Left Section */}
-
           <div className="flex items-center">
-            <a href="/my-profile" className="flex ms-2 md:me-24">
+            <Link to="/my-profile" className="flex ms-2 md:me-24">
               <img
                 src="https://navadhiti.com/static/logo-3cdf1dd56e7f958a1df40935db67c424.svg"
                 className="h-8 me-3"
@@ -22,19 +22,19 @@ function Navbar() {
               <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
                 Dashboard
               </span>
-            </a>
+            </Link>
           </div>
 
           {/* Right Section */}
           <div className="flex ms-3 space-x-4 items-center">
-            <a href="/notification">
+            <Link to="/notification">
               <IconButton
                 ariaLabel="Notifications"
                 className="bg-white-200 focus:ring-lime-600"
               >
                 <BellIcon className="w-8 h-8 p-1 text-shadow-gray-700 text-gray-700" />
               </IconButton>
-            </a>
+            </Link>
 
             <div className="relative">
               <IconButton
@@ -46,15 +46,15 @@ function Navbar() {
               </IconButton>
 
               <DropdownMenu isOpen={isUserMenuOpen}>
-                <a href="/my-profile" className={menuItemClass}>
+                <Link to="/my-profile" className={menuItemClass}>
                   My Profile
-                </a>
-                <a href="/my-answers" className={menuItemClass}>
+                </Link>
+                <Link to="/my-answers" className={menuItemClass}>
                   My Answers
-                </a>
-                <a href="/login" className={menuItemClass}>
+                </Link>
+                <Link to="/login" className={menuItemClass}>
                   Login
-                </a>
+                </Link>
               </DropdownMenu>
             </div>
           </div>
