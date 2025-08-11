a011b11b11b061c765380bc25bc39edaf0342cf9
Sindhu <sindhul25072002@gmail.com>
2025-08-11 19:38:24 +0530

    added checkbox element

---
+++ src/elements/Checkbox/checkboxdemo.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Checkbox from "./checkbox"
+
+function MyComponent() {
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <Checkbox
+      checked={checked}
+      onChange={(e) => setChecked(e.target.checked)}
+    />
+  );
+}
+export default MyComponent;
