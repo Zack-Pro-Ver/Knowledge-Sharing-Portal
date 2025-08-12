d05083a4047ec6cf7ea35cdfdf6060b1e09d2080
Medchal Bhavani <bhavanigithub5900@gmail.com>
2025-08-12 12:08:27 +0530

    removed the imports of button and card from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,6 @@
-import Button from "./elements/button";
-import Card, { CardContent, CardHeader } from "./elements/card";
-
+import React from "react";
 function App() {
-  return (
-    <>
-      <Button variant="contained" color="primary">
-        Click Me
-      </Button>
-      <Card>
-        <CardHeader
-          title="Card Title"
-          subheader="Card Subtitle"
-          avatar="🚀"
-          action={<Button variant="text">Action</Button>}
-        />
-
-        <CardContent>
-          <h3>Sample Card</h3>
-          <p>This is a sample card content</p>
-        </CardContent>
-      </Card>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
