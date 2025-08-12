a4704de0e7241ffcc930932cb110e2b32f80bb5b
Medchal Bhavani <bhavanigithub5900@gmail.com>
2025-08-12 11:56:01 +0530

    added card elememt with proper content

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import Button from "./elements/button";
-import DashboardLayout from "./layouts/dashboardlayout";
+import Card, { CardContent, CardHeader } from "./elements/card";
 
 function App() {
   return (
     <>
-      <DashboardLayout></DashboardLayout>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+      <Card>
+        <CardHeader
+          title="Card Title"
+          subheader="Card Subtitle"
+          avatar="🚀"
+          action={<Button variant="text">Action</Button>}
+        />
+
+        <CardContent>
+          <h3>Sample Card</h3>
+          <p>This is a sample card content</p>
+        </CardContent>
+      </Card>
     </>
   );
 }
