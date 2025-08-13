39aa5d389cf931633b56d9f6f3d1b82d7f225c56
Medchal Bhavani <bhavanigithub5900@gmail.com>
2025-08-13 18:44:50 +0530

    Refactor Sidebar component and update contents with icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-// import { Route, Routes, Link } from "react-router";
-
-// function Navbar() {
-//   return (
-//     <nav>
-//       <Link to="/">Home</Link>
-//       <Link to="/about">About</Link>
-//     </nav>
-//   );
-// }
-import Sidebar from "./components/Sidebar/index.tsx";
 function App() {
   return (
-    <Sidebar />
-    // <>
-    //   <Navbar/>
-    //    <div className="flex h-screen items-center justify-center bg-gradient-to-tr from-blue-400 to-purple-500">
-    //     <h1 className="text-4xl font-bold text-green-500">
-    //       Welcome to Tailwind CSS + Vite + React!
-    //     </h1>
-    //     </div>
-
-    //   <Routes>
-    //     {/* Define your routes here */}
-    //     <Route path="/" element={<div>Home Page</div>} />
-    //     <Route path="/about" element={<div>About Page</div>} />
-    //     {/* Add more routes as needed */}
-    //   </Routes>
-    // </>
+    <div className="flex h-screen items-center justify-center bg-gradient-to-tr from-blue-400 to-purple-500">
+      <h1 className="text-4xl font-bold text-green-500">
+        Welcome to Tailwind CSS + Vite + React!
+      </h1>
+    </div>
   );
 }
 
--- src/components/Sidebar/Sidebar.constant.ts
+++ src/components/Sidebar/Sidebar.constant.ts
@@ -1,7 +1,44 @@
-const SideBarContents = [
-    {
-        title: "Home",
-        icon: "home",
-        length: 4
-    }
+import {
+  RiUserLine,
+  RiDashboardLine,
+  RiQuestionLine,
+  RiChat1Line,
+  RiMedalLine,
+  RiNotificationLine,
+  RiSettingsLine,
+} from "@remixicon/react";
+
+export const SideBarContents = [
+  {
+    title: "Overview",
+    icon: RiDashboardLine,
+  },
+  {
+    title: "My Profile",
+    icon: RiUserLine,
+  },
+  {
+    title: "My Questions",
+    icon: RiQuestionLine,
+    length: 8,
+  },
+  {
+    title: "My Answers",
+    icon: RiChat1Line,
+    length: 7,
+  },
+  {
+    title: "Badges",
+    icon: RiMedalLine,
+    length: 9,
+  },
+  {
+    title: "Notifications",
+    icon: RiNotificationLine,
+    length: 0,
+  },
+  {
+    title: "Settings",
+    icon: RiSettingsLine,
+  },
 ];
