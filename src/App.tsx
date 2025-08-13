785010531064390c9b2bf703926259ac8da482f4
Medchal Bhavani <bhavanigithub5900@gmail.com>
2025-08-13 10:33:59 +0530

    app.tsx , renamed ChipRef.tsx and ButtonRef.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center bg-gradient-to-tr from-blue-400 to-purple-500">
+        <h1 className="text-4xl font-bold text-green-500">
+          Welcome to Tailwind CSS + Vite + React!
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
