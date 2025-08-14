b793efd4965d109f690a9c5ef444b0caf266a12a
Sindhu <sindhul25072002@gmail.com>
2025-08-14 16:21:45 +0530

    Changed path name Badges to badges

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import {Overview} from "./pages/Overview";
-import {Badges} from "./pages/Badges";
-import {MyAnswers} from "./pages/MyAnswers";
-import {MyQuestions} from "./pages/MyQuestions";
-import {MyProfile} from "./pages/MyProfile";
-import {Notification} from "./pages/Notifications";
+import { Overview } from "./pages/Overview";
+import { Badges } from "./pages/Badges";
+import { MyAnswers } from "./pages/MyAnswers";
+import { MyQuestions } from "./pages/MyQuestions";
+import { MyProfile } from "./pages/MyProfile";
+import { Notification } from "./pages/Notifications";
 import { ContentManagement } from "./pages/AdminControl/ContentManagement";
-import { ModerationQueue} from "./pages/AdminControl/ModerationQueue";
-import {UserManagement} from "./pages/AdminControl/UserManagement";
+import { ModerationQueue } from "./pages/AdminControl/ModerationQueue";
+import { UserManagement } from "./pages/AdminControl/UserManagement";
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -21,41 +21,37 @@ export const router = createBrowserRouter([
         element: <Overview />,
       },
       {
-        path:"Badges",
+        path: "badges",
         element: <Badges />,
       },
       {
-        path:"my-answers",
+        path: "my-answers",
         element: <MyAnswers />,
       },
       {
-        path:"my-questions",
+        path: "my-questions",
         element: <MyQuestions />,
       },
       {
-        path:"my-profile",
+        path: "my-profile",
         element: <MyProfile />,
       },
-      { 
-        path:"notifications",
+      {
+        path: "notifications",
         element: <Notification />,
       },
       {
-        path:"admin-control/content-management",
+        path: "admin-control/content-management",
         element: <ContentManagement />,
       },
       {
-        path:"admin-control/moderation-queue",
+        path: "admin-control/moderation-queue",
         element: <ModerationQueue />,
-      
       },
       {
-        path:"admin-control/user-management",
+        path: "admin-control/user-management",
         element: <UserManagement />,
       },
-      
-
-      
     ],
   },
 ]);
