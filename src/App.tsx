ecd65377348940d2f34d0e7b41260423df27178d
Jagadish Kesav <46987467+jagadishkesav@users.noreply.github.com>
2025-08-14 14:29:46 +0530

    Revert "installed react router dom package to navigate pages (#39)"
    This reverts commit 3b6c24f144da8dbc534967b4ba6b5ef6a54c8b6f.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div className="flex h-screen items-center justify-center bg-gradient-to-tr from-blue-400 to-purple-500">
+      <h1 className="text-4xl font-bold text-green-500">
+        Welcome to Tailwind CSS + Vite + React!
+      </h1>
+    </div>
+  );
 }
 
 export default App;
