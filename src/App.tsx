74f55dcc62b0774e030ee4f0977c7cca29ed2216
Medchal Bhavani <bhavanigithub5900@gmail.com>
2025-08-14 14:17:27 +0530

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-<<<<<<< HEAD
-import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    // <div className="flex h-screen items-center justify-center bg-gradient-to-tr from-blue-400 to-purple-500">
-    //   <h1 className="text-4xl font-bold text-green-500">
-    //     Welcome to Tailwind CSS + Vite + React!
-    //   </h1>
-    // </div>
-    <Sidebar />
+  <>
+    <div className="flex h-screen items-center justify-center bg-gradient-to-tr from-blue-400 to-purple-500">
+      <h1 className="text-4xl font-bold text-green-500">
+       Welcome to Tailwind CSS + Vite + React!
+     </h1>
+    </div>
+    
+  </>
   );
-=======
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
-
-function App() {
-  return <RouterProvider router={router} />;
->>>>>>> db97362f2bd2bc498cdfa2eae57e3665f6f0d490
 }
 
-export default App;
+export default App; 
