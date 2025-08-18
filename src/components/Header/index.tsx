1cecfd864a837d0f7543a862c43c23770ac15900
MedchalBhavani <162301343+MedchalBhavani@users.noreply.github.com>
2025-08-18 15:02:25 +0530

    Added an "Askquestion" header (#48)
    * added askquestion header
    
    * created the HeaderBar Section
    
    * Refactor Header component to make description optional and improve layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import Button from "../../elements/Button/index";
+
+interface Props {
+  title: string;
+  description?: string;
+  buttonText: string;
+  icon?: React.ReactNode;
+}
+export const Header = ({ title, description, buttonText, icon }: Props) => {
+  return (
+    <div className="flex flex-row justify-between p-4">
+      <div className="flex">
+        <p className="text-2xl font-bold ">{title}</p>
+        {description && <p className="text-base font-normal">{description} </p>}
+      </div>
+
+      <Button startIcon={icon}>{buttonText}</Button>
+    </div>
+  );
+};
+1;
