7e31ad558d478601f0b3bf1e28a6c96ea44bd56a
Sindhu <sindhul25072002@gmail.com>
2025-08-19 14:37:32 +0530

    Used tailwindcss @theme framework

---
+++ src/styles/Theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    primary: "#8E6EF4",   // Avatar background(Purple)
+    white: "#FFFFFF",     // Text on dark / avatar text
+    textDark: "#111827",  // gray-900(dark blue)
+    danger: "#DC2626",    // red-600 (notification badge)
+    border: "#E5E7EB",    // gray-200(blue shade)
+    darkBg: "#1F2937",    // gray-800 (header dark mode)
+  },
+};
