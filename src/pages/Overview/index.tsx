20afddea9658303c139f969b5661bbed8912e93f
Sindhu <sindhul25072002@gmail.com>
2025-08-19 10:03:19 +0530

    Added deleted header file

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -1,3 +1,16 @@
-export const Overview = () =>{
-  return <>Overview</>
-}
+import { RiQuestionLine } from "@remixicon/react";
+import { Header } from "../../components/Header";
+
+export const Overview = () => {
+  return (
+    <>
+      <Header
+        buttonText="Add Question"
+        // description="Here's what's happening with your account"
+        icon={<RiQuestionLine size="medium" />}
+        title="Welcome back, John!
+"
+      />
+    </>
+  );
+};
