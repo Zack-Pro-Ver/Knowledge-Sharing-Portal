c2a48c535cb8e72ec4a37b0f4aed231f341947dd
palani001 <palanisamy@navadhiti.com>
2025-08-21 16:00:39 +0530

    header section

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,13 @@ import Button from "../../elements/Button/index";
 interface Props {
   title: string;
   description?: string;
-  buttonText: string;
+  buttonText?: string;
   icon?: React.ReactNode;
 }
 export const Header = ({ title, description, buttonText, icon }: Props) => {
   return (
     <div className="flex flex-row justify-between p-4">
-      <div className="flex">
+      <div className="flex flex-col">
         <p className="text-2xl font-bold ">{title}</p>
         {description && <p className="text-base font-normal">{description} </p>}
       </div>
--- src/pages/MyAnswers/index.tsx
+++ src/pages/MyAnswers/index.tsx
@@ -1,3 +1,15 @@
-export const MyAnswers = () =>{
-  return <>My Answers</>
-}
+import { Search } from "lucide-react"; 
+import { Header } from "../../components/Header";
+
+export const MyAnswers = () => {
+  return (
+    <>
+      <Header
+        buttonText="Find Question"
+        description="Track your contributions and impact"
+        icon={<Search size={18} />}
+        title="My Answers"
+      />
+    </>
+  );
+};
