c08fcedd7f49e053b87e96d87952706ec1836990
Joicemercy78 <joicemercy7867@gmail.com>
2025-08-21 18:40:19 +0530

    create my notification page header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const Header = ({ title, description, buttonText, icon }: Props) => {
   return (
     <div className="flex flex-row justify-between p-4">
-      <div className="flex flex-col">
+      <div className="flex ">
         <p className="text-2xl font-bold ">{title}</p>
         {description && <p className="text-base font-normal">{description} </p>}
       </div>
--- src/pages/Notifications/index.tsx
+++ src/pages/Notifications/index.tsx
@@ -4,6 +4,7 @@ import { Header } from "../../components/Header"
 export const Notification = () =>{
 
   return <>
+
       <Header
         buttonText=" Mark All as Read "
         description=" Stay updated with your Q&A activity"
