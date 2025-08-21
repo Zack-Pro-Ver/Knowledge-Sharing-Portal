37ac60c18d2f9788251a8c3d78b35069a354de04
Joicemercy78 <joicemercy7867@gmail.com>
2025-08-21 18:15:23 +0530

    Created my notification page header

--- src/pages/Notifications/index.tsx
+++ src/pages/Notifications/index.tsx
@@ -1,3 +1,13 @@
+import { Header } from "../../components/Header"  
+
+
 export const Notification = () =>{
-  return <>Notification</>
+
+  return <>
+      <Header
+        buttonText=" Mark All as Read "
+        description=" Stay updated with your Q&A activity"
+        title="Notification"
+      />
+    </>
 }
