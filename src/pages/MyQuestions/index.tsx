98415ce465ad63e7f42ba3b913d349400f563f03
MohammedShameer10 <mdshameer41@gmail.com>
2025-08-21 15:53:37 +0530

    Created a header section for Question page

--- src/pages/MyQuestions/index.tsx
+++ src/pages/MyQuestions/index.tsx
@@ -1,3 +1,23 @@
-export const MyQuestions = () =>{
-  return <>My Questions</>
+import React from "react";
+import { Header } from "../../components/Header"; 
+import { FiPlus } from "react-icons/fi";
+
+interface MyQuestionsProps {
+  headerTitle?: string;
+  headerButtonText?: string;
 }
+
+export const MyQuestions: React.FC<MyQuestionsProps> = ({
+  headerTitle = "My Questions",
+  headerButtonText = "Ask Question",
+}) => {
+  return (
+    <div className="bg-gray-50 p-4">
+      <Header
+        title={headerTitle}
+        buttonText={headerButtonText}
+        icon={<FiPlus />} 
+      />
+    </div>
+  );
+};
