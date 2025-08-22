8b81c516720496dcb9a1a505c1ce0ad9a47c1d5b
Kavisindhu-N <kavisindhunatarajan@gmail.com>
2025-08-22 16:59:39 +0530

    created header for badges page (#89)
    * created header for badges page
    
    * added the suggested changes
    
    * Update package-lock.json
    
    * Update package-lock.json
    
    * added conditional rendering on Button

--- src/pages/Badges/index.tsx
+++ src/pages/Badges/index.tsx
@@ -1,3 +1,11 @@
-export const Badges = () =>{
-  return <>Badges</>
+import { Header } from "../../components/Header"
+
+export const Badges = () => {
+  return (
+    <Header
+      title='My Badges'
+      description="Track your achievements and progress"
+      showButton={false}
+    />
+  )
 }
