a9671204d3e855f4dfeaf8f80a4f8edbb75ea1b9
Kavisindhu-N <kavisindhunatarajan@gmail.com>
2025-08-25 22:08:57 +0530

    made suggested changes-using Button element

--- src/elements/Tab/index.tsx
+++ src/elements/Tab/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import type { TabsProps } from "./Tab.types";
 import { tabClasses, getTabStyles } from "./Tab.styles";
+import Button from "../Button";
 
 const Tab: React.FC<TabsProps> = ({ tabs, activeIndex, onTabChange, className }) => {
   return (
     <nav className={`${tabClasses.root} ${className || ""}`}>
       {tabs.map((tab, index) => (
-        <button
+        <Button
           key={index}
           className={getTabStyles(index === activeIndex)}
           onClick={() => onTabChange(index)}
         >
           {tab.label}
-        </button>
+        </Button>
       ))}
     </nav>
 
