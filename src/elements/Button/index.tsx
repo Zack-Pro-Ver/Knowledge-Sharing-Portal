b77a58b67adce8fe99bb5c8a26029672fa0142f2
Medchal Bhavani <bhavanigithub5900@gmail.com>
2025-08-25 16:40:36 +0530

    loading icon addition to the button to work when load is done

--- src/elements/Button/index.tsx
+++ src/elements/Button/index.tsx
@@ -25,6 +25,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
     ...otherProps
   } = props;
 
+  // Get styles from your helper functions
   const buttonStyles = getButtonStyles(
     variant,
     size,
@@ -41,37 +42,24 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   return (
     <button
       ref={ref}
-      className={`${buttonStyles} ${className}`}
+      className={`${buttonStyles} ${className} relative flex items-center justify-center`}
       disabled={disabled || loading}
       onClick={onClick}
       {...otherProps}
     >
+      {/* Loading spinner using Remix Icon */}
       {loading && (
-        <span className={loadingStyles}>
-          <svg
-            className={`${iconStyles} animate-spin`}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            />
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            />
-          </svg>
+        <span className={`${loadingStyles} absolute left-4`}>
+          <i className={`${iconStyles} animate-spin ri-loader-4-line`}></i>
         </span>
       )}
 
-      <span className={`${labelStyles} ${loading ? "opacity-0" : ""}`}>
+      {/* Button content */}
+      <span
+        className={`${labelStyles} ${
+          loading ? "opacity-0" : ""
+        } flex items-center`}
+      >
         {startIcon && <span className={iconStyles}>{startIcon}</span>}
         {children}
         {endIcon && <span className={iconStyles}>{endIcon}</span>}
