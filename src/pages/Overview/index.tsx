4d8fc5ab401ce963ed527b03922d5e14629b8e4f
Medchal Bhavani <bhavanigithub5900@gmail.com>
2025-08-25 13:08:48 +0530

    added card

--- src/pages/Overview/index.tsx
+++ src/pages/Overview/index.tsx
@@ -1,16 +1,19 @@
 import { RiQuestionLine } from "@remixicon/react";
 import { Header } from "../../components/Header";
+import { RecentAnswersCard } from "./RecentAnswers"; // Import RecentAnswersCard
 
 export const Overview = () => {
   return (
     <>
       <Header
         buttonText="Add Question"
+        showButton={true} // Add showButton property
         // description="Here's what's happening with your account"
         icon={<RiQuestionLine size="medium" />}
         title="Welcome back, John!
 "
       />
+      <RecentAnswersCard />
     </>
   );
 };
