0b9bd6b1ea9c4157933c082a944935961292b812
Kathir2315 <kathirmskgt@gmail.com>
2025-08-26 07:17:09 +0530

    Create notification cards for Notification page

--- src/pages/MyAnswers/index.tsx
+++ src/pages/MyAnswers/index.tsx
@@ -1,15 +1,3 @@
-import { RiSearchLine } from "@remixicon/react";
-import { Header } from "../../components/Header";
-
-export const MyAnswers = () => {
-  return (
-    <>
-      <Header
-        buttonText="Find Question"
-        description="Track your contributions and impact"
-        icon={<RiSearchLine className="absolute left-4 item-center text-white w-5 h-5"/>}
-        title="My Answers"
-      />
-    </>
-  );
-};
+export const MyAnswers = () =>{
+  return <>My Answers</>
+}
